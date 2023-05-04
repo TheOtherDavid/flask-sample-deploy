e60316c3bd11d0fc50cfe16f97ac62166bf19e8a
David Hand <davidhand1988@gmail.com>
2023-05-04 16:22:16 -0400

    More changes.

--- myproject.py
+++ myproject.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:red'>Hello There!</h1>"
 
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
