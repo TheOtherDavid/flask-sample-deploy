9ddeb8595161d0ded6a5955e1edc39935d42eab0
David Hand <davidhand1988@gmail.com>
2023-05-04 22:05:11 -0400

    Add logging.

--- myproject.py
+++ myproject.py
@@ -1,13 +1,22 @@
 from flask import Flask
+print("Importing Flask")
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    print("Handling the hello route")
     return "<h1 style='color:red'>Hello There!</h1>"
 
 @app.route('/health')
 def health_check():
+    print("Handling the health_check route")
     return 'OK', 200
 
+def create_app():
+    print("Creating the Flask app")
+    return app
+
 if __name__ == "__main__":
+    print("Running the app directly")
     app.run(host='0.0.0.0', port=8080)
